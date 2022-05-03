720f511381a53fffb006b4bab3f61245c6862f5d
Mahesh1997 <Mahesh1997@MaheshMCapgemin>
2022-05-03 21:48:28 +0530

    Add project files.

---
+++ Day6MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day6MvcApp.Models;
+
+namespace Day6MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(10000);
+            return PartialView();
+        }
+
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+
+
+
+    }
+}
